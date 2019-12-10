d3f7bf7ad43496c08b6e8881536d9c98ced11d3d
Vince <vincent.lagrassa@gmail.com>
2019-12-10 11:11:32 -0600

    Use instances for associationmatrix only

--- organism.hs
+++ organism.hs
@@ -36,28 +36,26 @@ data Organism = Organism {
     deriving (Show, Eq)
 
 
-instance Enum Organism where
-    toEnum n = Organism assoc [1,1,1,1,1]
+instance Enum AssociationMatrix where
+    toEnum n = fromList_a . reverse . make_bin_list n $ num_signals * num_objects
         where
-            assoc = fromList_a . reverse . make_bin_list n $ num_signals * num_objects
-
             -- Decompose input into list of 0s and 1s based on binary representation
             make_bin_list _ (-1) = []
             make_bin_list n index = if (n < 2 ^ index)
                 then 0 : make_bin_list n (index-1)
                 else 1 : make_bin_list (n - 2^index) (index-1)
 
-    fromEnum = fromInteger . fst . foldl parse_bin_list (0,0) . getOrgMatrix
+    fromEnum = fromInteger . fst . foldl parse_bin_list (0,0) . getMatrix
         where
             parse_bin_list (acc, ind) curr = (acc + curr * (2 ^ ind), ind+1)
 
 
-instance Bounded Organism where
-    minBound = Organism (matrix_a $ \(i,j) -> 0) [1,1,1,1,1]
-    maxBound = Organism (matrix_a $ \(i,j) -> 1) [1,1,1,1,1]
+instance Bounded AssociationMatrix where
+    minBound = matrix_a $ \(i,j) -> 0
+    maxBound = matrix_a $ \(i,j) -> 1
 
 
-instance Random Organism where
+instance Random AssociationMatrix where
     randomR (a, b) g = 
         case randomR (fromEnum a, fromEnum b) g of
             (o, g') -> (toEnum o, g')
