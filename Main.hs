0208b7b6c70c246e9d36bbe8e6cf380f861c15ee
Vince <vincent.lagrassa@gmail.com>
2019-12-11 16:48:02 -0600

    Standardize normalization

--- Main.hs
+++ Main.hs
@@ -11,13 +11,19 @@ import Utils
 type Population = [Organism]
 
 
-payoff_percent :: Fractional a => Organism -> Organism -> a
-payoff_percent = curry $ normalize_payoff . uncurry payoff
-
-normalize_payoff :: Fractional a => Rational -> a
-normalize_payoff = (flip (/)) maxPayoff . fromRational
+-- Payoff values normalized such that 1.0 is the maximum possible payoff
+norm_payoff :: Fractional a => Rational -> a
+norm_payoff = (flip (/)) maxPayoff . fromRational
     where
         maxPayoff = realToFrac $ min num_signals num_objects
+
+normalize_f :: Fractional c => (a -> b -> Rational) -> a -> b -> c
+normalize_f f = curry $ norm_payoff . uncurry f
+
+
+-- The payoff for successful communication between two organisms, normalized such that 1.0 is the maximum possible payoff
+payoff_n :: Fractional a => Organism -> Organism -> a
+payoff_n = normalize_f payoff
 
 
 -- The average payoff for an organism within a population
@@ -27,11 +33,17 @@ fitness pop i = sum payoffs / (realToFrac $ length pop - 1)
         payoffs = [(payoff (pop !! i) (pop !! x)) | x <- indices , x /= i]
         indices = [0 .. length pop - 1]
 
+fitness_n :: Fractional a => Population -> Int -> a
+fitness_n = normalize_f fitness
+
 
 -- An array of the fitness of each organism in the population
 fitness_arr :: Population -> [Rational]
 fitness_arr pop = fmap (fitness pop) [0 .. length pop - 1]
 -- (flip fmap) [0 .. length pop - 1] . avg_payoff_org
+
+fitness_arr_n :: Fractional a => Population -> [a]
+fitness_arr_n = fmap norm_payoff . fitness_arr
 
 
 -- The average fitness of the population
@@ -39,6 +51,10 @@ avg_fitness :: Population -> Rational
 avg_fitness pop = sum . fmap (flip (/) (toRational . length $ arr)) $ arr
     where
         arr = fitness_arr pop
+
+avg_fitness_n :: Fractional a => Population -> a
+avg_fitness_n = norm_payoff . avg_fitness
+
 
 
 -- A list of all the unique association matrices in the population
