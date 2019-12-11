7b0598baa74a7d02aba2c33169c973bb28809859
Vince <vincent.lagrassa@gmail.com>
2019-12-11 00:11:20 -0600

    Utility function to zip matrices

--- utils.hs
+++ utils.hs
@@ -16,6 +16,12 @@ normalizeCols m = mapIndex normalizeCol m
     where
         normalizeCol _ j n = n / (sum . getCol j $ m)
 
+zipMatrices :: (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
+zipMatrices f a b = matrix num_rows num_cols $ \(i,j) -> f (a ! (i,j)) (b !(i,j))
+    where
+        num_rows = min (nrows a) (nrows b)
+        num_cols = min (ncols a) (ncols b)
+
 
 -- Round a number f to the nth place
 roundN :: (RealFrac a1, Integral b, Fractional a2) => b -> a1 -> a2
