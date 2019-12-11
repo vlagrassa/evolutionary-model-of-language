963f2f8b7e67ddf5668efee620edb37891b2e072
Vince <vincent.lagrassa@gmail.com>
2019-12-10 23:55:03 -0600

    Move matrices and payoff to organism file

--- Main.hs
+++ Main.hs
@@ -11,49 +11,6 @@ import Utils
 type Population = [Organism]
 
 
--- In the paper, this is denoted with P
--- Probability of using signal j for object i
-send_matrix :: Organism -> Matrix Rational
-send_matrix = normalizeRows . fmap assocToRational . getOrgMatrix
-
--- In the paper, this is denoted with Q
--- Probability of interpreting signal j as refering to object i
-hear_matrix :: Organism -> Matrix Rational
-hear_matrix = normalizeCols . fmap assocToRational . getOrgMatrix
-
-
--- In the paper, this is denoted with F (Adapts equations 4 and 8)
--- The average payoff of either organism succesfully communicating an arbitrary message to the other
-payoff :: Organism -> Organism -> Rational
-payoff a b = ((total_payoff a b) + (total_payoff b a)) / 2
-    where
-        -- Total payoff for X succesfully communicating with Y
-        total_payoff x y = sum [success_payoff i x y | i <- [1..num_objects]]
-
-        -- Payoff for X succesfully communicating "i" to Y
-        success_payoff i x y = (comm_payoff x !! (i-1)) * (comm_success i x y)
-
-
--- Probability of X succesfully communicating "i" to Y
-comm_success :: Int -> Organism -> Organism -> Rational
-comm_success i x y = sum success_array
-    where
-        -- Probability of successful communication using each signal
-        success_array = [prob_success j k | j <- [1..num_signals], k <- [1..num_signals]]
-
-        -- Probability of successful communication where X sends j and Y hears k
-        prob_success j k = (prob_send j) * (prob_error j k) * (prob_hear k)
-
-        -- Probability that the sender will denote object "i" with signal "j"
-        prob_send j = send_matrix x ! (i,j)
-
-        -- Probability that listener will hear signal "j" as signal "k"
-        prob_error j k = toRational $ error_matrix ! (j,k)
-
-        -- Probability that listener will infer object "i" from signal "k"
-        prob_hear k = hear_matrix y ! (i,k)
-
-
 payoff_percent :: Fractional a => Organism -> Organism -> a
 payoff_percent = curry $ normalize_payoff . uncurry payoff
 
