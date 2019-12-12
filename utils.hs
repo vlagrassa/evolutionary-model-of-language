101f1be0165ce6217c4635fbf2227bec8fc23802
Vince <vincent.lagrassa@gmail.com>
2019-12-12 01:15:54 -0600

    Handle case where row or col is all 0s

--- utils.hs
+++ utils.hs
@@ -9,12 +9,14 @@ mapIndex f m = matrix (nrows m) (ncols m) $ \(i,j) -> f i j (m ! (i,j))
 normalizeRows :: RealFrac a => Matrix a -> Matrix a
 normalizeRows m = mapIndex normalizeRow m
     where
-        normalizeRow i _ n = n / (sum . getRow i $ m)
+        normalizeRow i _ n = if total i == 0 then 0 else n / (total i)
+        total i = sum . getRow i $ m
 
 normalizeCols :: RealFrac a => Matrix a -> Matrix a
 normalizeCols m = mapIndex normalizeCol m
     where
-        normalizeCol _ j n = n / (sum . getCol j $ m)
+        normalizeCol _ j n = if total j == 0 then 0 else n / (total j)
+        total j = sum . getCol j $ m
 
 zipMatrices :: (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
 zipMatrices f a b = matrix num_rows num_cols $ \(i,j) -> f (a ! (i,j)) (b !(i,j))
